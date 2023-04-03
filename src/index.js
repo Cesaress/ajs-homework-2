88a4f796c5a59ddd4e5fc631fef8fed2b277987a
Андрей Богданов <cesaress@yandex.ru>
2023-04-03 20:39:16 +0300

    HW 2 rev

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './css/style.css';
 import './js/app.js';
 import './js/domain.js';
 import './js/game.js';
+
+console.log('app worked');
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,44 @@
 const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader'
-                ]
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'hw.bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new MiniCssExtractPlugin()
-    ]
-}
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
